064a050426a4327c3a83bc00fba31901dd820ef1
BangBoyGI <148872368+BangBoyGI@users.noreply.github.com>
2023-10-24 20:02:57 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        loader: 'akamai',
+        path: '',
         remotePatterns: [
             {
                 hostname: "cdn.myanimelist.net"
             }
         ]
-    }
+    },
+    assetPrefix: './',
 }
 
 module.exports = nextConfig
